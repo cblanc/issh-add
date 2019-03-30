b604eca3cdbe341b618d6fd81cb9a592b82375e6
Christopher Blanchard <cablanchard@gmail.com>
2019-03-30 16:34:38 +0100

    Verify parser works

--- test/test.ts
+++ test/test.ts
@@ -1,11 +1,15 @@
+/* tslint:disable:no-magic-numbers */
+
 import { assert } from "chai";
 import { parseConfig } from "../lib/index";
 import { resolve } from "path";
 
 describe("Parse config", () => {
   it("extracts config from absolute path", async () => {
-    const path = resolve(__dirname, "./fixtures/ssh_config");
-    const config = await parseConfig(path);
-    process.stdout.write(JSON.stringify(config));
+    const path = resolve(__dirname, "./fixtures/config");
+    const [first, second, third] = await parseConfig(path);
+    assert.equal(first.value, "dev");
+    assert.equal(second.value, "github.com");
+    assert.equal(third.value, "*");
   });
 });
