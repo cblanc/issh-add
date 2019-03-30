6fb2acc2aeff5f7e45dfb5cde47aaa2a4f05c971
Christopher Blanchard <cablanchard@gmail.com>
2019-03-30 17:40:20 +0100

    Handle cases where a top level identifyfile has been declared

--- lib/cli.ts
+++ lib/cli.ts
@@ -28,30 +28,66 @@ interface KeyValueObject {
   [key: string]: string;
 }
 
+interface ConfigParser {
+  (sshConfig: SshConfig): HostConfiguration | void;
+}
+
+const identityFileSplitter = (i: string): string[] => i.split(/\s+/);
+
 /**
- * toHostConfig
+ * extractIdentifyFile
+ *
+ * Iterates over relevant identifyfile objects and merges into a single array
  */
-export const toHostConfig = (sshConfig: SshConfig): HostConfiguration => {
-  const hostConfig: HostConfiguration = { identityFile: [] };
+const extractIdentifyFile = (sshConfig: SshConfig): string[] => {
+  const { config } = sshConfig;
+  return config
+    .filter(({ param }) => param.toLowerCase() === "identityfile")
+    .reduce<string[]>(
+      (result, { value }) => result.concat(identityFileSplitter(value)),
+      []
+    );
+};
 
-  // Parent value is host
-  hostConfig.host = sshConfig.value;
-
-  const config = sshConfig.config.reduce<KeyValueObject>((prev, curr) => {
-    prev[curr.param.toLowerCase()] = curr.value;
+/**
+ * mergeConfigurations
+ *
+ * Merges array of SshConfiguration objects into a single config object
+ */
+const mergeConfigurations = (sshConfig: SshConfig): KeyValueObject => {
+  return sshConfig.config.reduce<KeyValueObject>((prev, curr) => {
+    const { value, param } = curr;
+    const attr = param.toLowerCase();
+    // Ignore identifyfile as this needs to be handled separately
+    if (attr !== "identityfile") prev[attr] = value;
     return prev;
   }, {});
+};
 
+const parseHostBlock: ConfigParser = sshConfig => {
+  const hostConfig: HostConfiguration = {
+    // Parent value is host
+    host: sshConfig.value,
+    identityFile: extractIdentifyFile(sshConfig),
+  };
+
+  const config = mergeConfigurations(sshConfig);
   if (config.host) hostConfig.host = config.host;
   if (config.hostname) hostConfig.hostName = config.hostname;
   if (config.user) hostConfig.user = config.user;
   if (config.port) hostConfig.port = config.port;
 
-  // Load in identity file as array of strings
-  if (config.identityfile)
-    hostConfig.identityFile = config.identityfile.split(/\s+/);
-
   return hostConfig;
+};
+
+/**
+ * toHostConfig
+ */
+export const toHostConfig: ConfigParser = sshConfig => {
+  const param = sshConfig.param.toLowerCase();
+  if (param === "host") return parseHostBlock(sshConfig);
+  // Return void if invalid block detected
+  return;
 };
 
 /**
