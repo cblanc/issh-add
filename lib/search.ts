91b95f7f1157940a439b2da38ace4681881868df
Christopher Blanchard <cablanchard@gmail.com>
2019-03-31 16:20:51 +0200

    Improve results

--- lib/search.ts
+++ lib/search.ts
@@ -1,15 +1,26 @@
 import Fuse from "fuse.js";
 import { HostConfiguration } from "./index";
 
+interface Options {
+  shouldSort: boolean;
+  minMatchCharLength: number;
+  location: number;
+  distance: number;
+  tokenize: boolean;
+  keys: string[];
+}
+
+const options: Options = {
+  shouldSort: true,
+  minMatchCharLength: 1,
+  location: 0,
+  distance: 0,
+  tokenize: false,
+  keys: ["identityFile", "host", "hostName", "user"],
+};
+
 export const generateIndex = (
   configs: HostConfiguration[]
-): Fuse<HostConfiguration> => {
-  return new Fuse(configs, {
-    shouldSort: true,
-    minMatchCharLength: 3,
-    location: 0,
-    distance: 0,
-    tokenize: true,
-    keys: ["identityFile", "host", "hostName", "user"],
-  });
+): Fuse<HostConfiguration, Options> => {
+  return new Fuse(configs, options);
 };
