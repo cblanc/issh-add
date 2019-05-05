ca00a5cf5797426464441f5694f0803373989d89
Christopher Blanchard <cablanchard@gmail.com>
2019-05-05 11:16:51 +0200

    Implement agent detection

--- lib/ssh-agent.ts
+++ lib/ssh-agent.ts
@@ -1,6 +1,20 @@
 import { spawnSync, SpawnSyncReturns } from "child_process";
 import { homedir } from "os";
 
+const { SSH_AUTH_SOCK, SSH_AGENT_PID } = process.env;
+
+process.stdout.write(JSON.stringify(process.env));
+
+interface Agent {
+  SSH_AUTH_SOCK: string;
+  SSH_AGENT_PID: string | undefined;
+}
+
+export const getAgent = (): Agent | undefined => {
+  if (SSH_AUTH_SOCK === undefined) return;
+  return { SSH_AUTH_SOCK, SSH_AGENT_PID };
+};
+
 const normalise = (file: string): string => {
   const homedirRegex = /^~/;
   if (file.match(homedirRegex) !== null) {
